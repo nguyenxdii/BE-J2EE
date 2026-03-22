32ecb7da864a859376710e9e14a64bb3bff7cc35
zxckhanh <kvolleyballzz23@gmail.com>
2026-03-23 02:14:55 +0700

    feat: khởi tạo cấu trúc thư mục và các file service cho 6 chức năng Admin và User

--- src/main/java/com/j2ee/carbooking/CarbookingApplication.java
+++ src/main/java/com/j2ee/carbooking/CarbookingApplication.java
@@ -2,7 +2,6 @@ package com.j2ee.carbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
